36c1ac7c083fbf1da4e4d73efa8cb0f5f2bb7891
Shubhankar30 <shubhankarranade30@gmail.com>
2018-05-19 18:06:39 +0900

    SweetAlert integration

---
+++ assets/js/custom.js
@@ -0,0 +1,71 @@
+$( "#hapticserpent" ).on( "click", function() {
+    swal({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: 'https://unsplash.it/400/200',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+        animation: false
+      })
+})
+
+$( "#clashtanks" ).on( "click", function() {
+    swal({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: 'https://unsplash.it/400/200',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+        animation: false
+      })
+})
+
+$( "#smart-museum-design" ).on( "click", function() {
+    swal({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: 'https://unsplash.it/400/200',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+        animation: false
+      })
+})
+
+$( "#sticky-header" ).on( "click", function() {
+    swal({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: 'https://unsplash.it/400/200',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+        animation: false
+      })
+})
+
+$( "#simple-dictionary" ).on( "click", function() {
+    swal({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: 'https://unsplash.it/400/200',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+        animation: false
+      })
+})
+
+$( "#life-in-weeks" ).on( "click", function() {
+    swal({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: 'https://unsplash.it/400/200',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+        animation: false
+      })
+})
