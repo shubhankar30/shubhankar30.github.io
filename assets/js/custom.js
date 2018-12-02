de4b9c2e6cc750ea8f42436bba09c0b3f67cea53
Shubhankar30 <shubhankarranade30@gmail.com>
2018-12-02 22:25:17 +0900

    Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,26 +18,18 @@ el.innerText = "Click here"
 $( "#open-dictionary" ).on( "click", function() {
     swal({
         title: 'Open Dictionary',
-        text: 'Modal with a custom image.',
+        text: 'Simplistic, open-source on the go dictionary in the form of a android application.',
         animation: true,
-        html: '<a href="https://www.researchgate.net/publication/324668531_HapticSerpent_A_Wearable_Haptic_Feedback_Robot_for_VR">Read more</a>',
+        html: '<a href="https://github.com/shubhankar30/OpenDictionary">Github Repository Link</a>',
       })
 })
 
 $( "#english-marathi-dictionary" ).on( "click", function() {
     swal({
         title: 'English Marathi Dictionary',
-        text: 'Modal with a custom image.',
+        text: 'An Android Application to find meanings from English to Marathi and vice versa.',
         animation: true,
-        html: '<a href="https://www.researchgate.net/publication/324668531_HapticSerpent_A_Wearable_Haptic_Feedback_Robot_for_VR">Read more</a>',
+        html: '<a href="https://github.com/shubhankar30/Marathi-English-Dictionary">Github Repository Link</a>',
       })
 })
 
-$( "#ebu" ).on( "click", function() {
-    swal({
-        title: 'EBU',
-        text: 'Modal with a custom image.',
-        animation: true,    
-        html: '<a href="https://www.researchgate.net/publication/324668531_HapticSerpent_A_Wearable_Haptic_Feedback_Robot_for_VR">Read more</a>',
-    })
-})
